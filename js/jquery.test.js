e689cf8c23a98e8444732c5f6b188bb549423ca9
berserker527 <berserker527@sina.com>
2014-11-03 14:27:40 +0800

    a

--- js/jquery.test.js
+++ js/jquery.test.js
@@ -1,3 +1,11 @@
-$.fn.showInfo = function(){
-	alert($(this).html);
-}
+jQuery.fn.extend({
+	isSupportTouch : "ontouchend" in document ? true : false,
+	swipeLeft : function(fn){
+		if(isSupportTouch){
+			$(this).on("touchmove",fn);
+		}
+	},
+	cc : function(fn){
+		$(this).click(fn);
+	}
+});
