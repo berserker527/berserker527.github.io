53cd973670e128b0e96c504c86de13e51e4debe4
berserker527 <berserker527@sina.com>
2014-11-03 16:49:44 +0800

    a

--- js/jquery.test.js
+++ js/jquery.test.js
@@ -1,25 +1,38 @@
 var _isSupportTouch = "ontouchend" in document ? true : false;
 var _startX, _startY, _endX, _endY;
+var _direction;
 jQuery.fn.extend({
-	swipe : function(){
-		$(this).one("touchstart",function(e){
-			_startX = e.pageX;
-			_startY = e.pageY;
-		}).one("touchend",function(e){
-			_endX = e.pageX;
-			_endY = e.pageY;
-		});
-		if(_startX < _endX){
-			alert("right");
+	swipe : function(fn, direction){
+		if (!_isSupportTouch) {
+			return this;
 		}
-		else{
-			alert("left");
+		if(direction == "right" || direction == "left"){
+			$(this).on("touchstart",function(e){
+				_startX = e.pageX;
+				_startY = e.pageY;
+				console.log(_startX + " " + _startY);
+			}).on("touchend",function(e){
+				_endX = e.pageX;
+				_endY = e.pageY;
+				console.log(_endX + " " + _endY);
+				if(_startX < _endX){
+					_direction = "right";
+				}
+				else if(_startX > _endX){
+					_direction = "left";
+				}
+				if(_direction == direction){
+					fn;
+				}
+			});
 		}
+		return this;
 	},
 	swipeLeft : function(fn){
-		if(_isSupportTouch){
-			$(this).on("touchmove",fn);
-		}
+		swipe(fn,"left");
+	},
+	swipeRight : function(fn){
+		swipe(fn,"right");
 	},
 	cc : function(fn){
 		if(fn){
