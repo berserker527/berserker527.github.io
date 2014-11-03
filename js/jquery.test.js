0b185dc7a786b3be6d7660646c2d645a9a88fd71
berserker527 <berserker527@sina.com>
2014-11-03 15:16:50 +0800

    a

--- js/jquery.test.js
+++ js/jquery.test.js
@@ -1,11 +1,16 @@
-var isSupportTouch = "ontouchend" in document ? true : false;
+var _isSupportTouch = "ontouchend" in document ? true : false;
+var _startX, _startY, _endX, _endY;
 jQuery.fn.extend({
+	swipe : function(fn){
+		
+	},
 	swipeLeft : function(fn){
-		if(isSupportTouch){
+		if(_isSupportTouch){
 			$(this).on("touchmove",fn);
 		}
 	},
 	cc : function(fn){
+		alert(this.e.pageX + " " + this.e.pageY);
 		$(this).click(fn);
 	}
 });
