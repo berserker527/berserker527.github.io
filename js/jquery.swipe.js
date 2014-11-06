5d61c48fee43e7ec6ce122591e5749991af61deb
berserker527 <berserker527@sina.com>
2014-11-06 11:44:09 +0800

    a

--- js/jquery.swipe.js
+++ js/jquery.swipe.js
@@ -91,8 +91,6 @@ var pos = {
 jQuery.fn.extend({
 	swipe : function(fn, _direction ,_distance){
 //		console.log("--swipe");
-		e = window.event || event || arguments[0];console.log(e);
-		fn = fn.call(e.target, e);
 		var eventParam = {}
 		if(_distance){
 			com.minDistance = _distance > 0 ? _distance : 1;
