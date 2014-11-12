0b3d6d98675cdbd8dce37fe28707ad643352f56c
berserker527 <berserker527@sina.com>
2014-11-12 17:14:52 +0800

    a

--- js/gwql.js
+++ js/gwql.js
@@ -5,9 +5,29 @@ $(function(){
 	var trace = temp[temp.length - 1].replace(".html","");
 	var _width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	if(_width >= 1024){
-		console.log("ok");
-		console.log($("#" + disk).html());
-		$("#" + disk).show();
+		$("#" + disk).parent().show();
 	}
+	
+	$(".disk-option li").click(function(){
+		if (_width >= 1024) {
+			var index = $(this).index();
+			$(".trace").hide().eq(index).show();
+		}
+	})
+	
+	$(".disk-li").hover(
+		function(){
+			console.log(_width);
+			if (_width < 1024) {
+				$(this).children(".trace-down").show();
+			}
+		},
+		function(){
+			console.log(_width);
+			if (_width < 1024) {
+				$(this).children(".trace-down").hide();
+			}
+		}
+	);
 
 });
