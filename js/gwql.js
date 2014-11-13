2c268fdee55f4318d2243007c62db087c3f8ae02
berserker527 <berserker527@sina.com>
2014-11-13 15:41:35 +0800

    a

--- js/gwql.js
+++ js/gwql.js
@@ -4,8 +4,11 @@ $(function(){
 	var disk = temp[temp.length - 2];
 	var trace = temp[temp.length - 1].replace(".html","");
 	var _width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	if(_width >= 1024){
-		$("#" + disk).parent().show();
+	function initTitle(){
+		if(_width >= 1024){
+			$("#" + disk).parent().show();
+			$("ul[disk=" + disk + "]").children("li[trace=" + trace + "]").append("<span></span>");
+		}
 	}
 	
 	$(".disk-option li").click(function(){
@@ -29,5 +32,61 @@ $(function(){
 			}
 		}
 	);
+	
+	var sectionCount;
+	var pageCount;
+	var currentPage = 0;
+	function initPager(){
+		$.ajax({
+          type: "GET",
+          url: "../txt/" + disk + "_" + trace + ".txt",
+          success: function(data){
+		  	sectionCount = data.split("<br/>").length;
+			pageCount = (sectionCount - 1) / 5;
+			
+			for (var i = 0; i < pageCount; i++) {
+				if(i == 0){
+					$(".pager").append('<div class="pageNum current" index="' + i + '">' + (i + 1) + '</div>');
+				}
+				else{
+					$(".pager").append('<div class="pageNum" index="' + i + '">' + (i + 1) + '</div>');
+				}
+			};
+			
+			
+			$(".pageNum").click(function(){
+				$(".pageNum").removeClass("current");
+				$(this).addClass("current");
+				var index = $(this).attr("index");
+				showContext(index);
+				window.document.body.scrollTop = 0;
+			});
+          }
+        });
+	}
+	
+	function showContext(currentPage){
+		$.ajax({
+          type: "GET",
+          url: "../txt/" + disk + "_" + trace + ".txt",
+          success: function(data){
+			data = data.split("<br/>");
+			
+			$(".context").html("");
+			for (var i = 0; i < 5; i++) {
+				if(5 * currentPage + i < sectionCount){
+					$(".context").append(data[5 * currentPage + i] + "<br/>")
+				}
+				else{
+					break;
+				}
+			};
+          }
+        });  
+	}
+	 
+	initTitle();
+	//initPager();
+	showContext(0);
 
 });
