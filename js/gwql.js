1213e8432f7448d00f0db89c4dcf15678809a5d9
berserker527 <berserker527@sina.com>
2014-11-13 17:24:43 +0800

    a

--- js/gwql.js
+++ js/gwql.js
@@ -1,20 +1,58 @@
 $(function(){
+	var disks = {"disk1":"Disk 1 苏醒的梦", "disk2":"Disk 2 流转的风", "disk1":"Disk 3 消散的虹"}
+	var tracks = {"disk1":{
+		"track1":"Track 1 弦拨浪起",
+		"track2":"Track 2 我的前奏弹给你听",
+		"track3":"Track 3 吟咏",
+		"track4":"Track 4 破音",
+		"track5":"Track 5 混乱的狂想曲",
+		"track6":"Track 6 哀鸿调",
+		"track7":"",
+		"track8":"",
+		"track9":"",
+		"track10":""
+	},"disk2":{
+		"track1":"",
+		"track2":"",
+		"track3":"",
+		"track4":"",
+		"track5":"",
+		"track6":"",
+		"track7":"",
+		"track8":"",
+		"track9":"",
+		"track10":""
+	},"disk3":{
+		"track1":"",
+		"track2":"",
+		"track3":"",
+		"track4":"",
+		"track5":"",
+		"track6":"",
+		"track7":"",
+		"track8":"",
+		"track9":"",
+		"track10":""
+	}};
 	var url = window.location.href;
 	var temp = url.split("/");
 	var disk = temp[temp.length - 2];
-	var trace = temp[temp.length - 1].replace(".html","");
+	var track = temp[temp.length - 1].replace(".html","");
 	var _width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	function initTitle(){
+		document.title = "歌尾倾泪_" + tracks[disk][track];
+		$(".title-disk").text(disks[disk]);
+		$(".title-track").text(tracks[disk][track]);
 		if(_width >= 1024){
 			$("#" + disk).parent().show();
-			$("ul[disk=" + disk + "]").children("li[trace=" + trace + "]").append("<span></span>");
+			$("ul[disk=" + disk + "]").children("li[track=" + track + "]").append("<span></span>");
 		}
 	}
 	
 	$(".disk-option li").click(function(){
 		if (_width >= 1024) {
 			var index = $(this).index();
-			$(".trace").hide().eq(index).show();
+			$(".track").hide().eq(index).show();
 		}
 	})
 	
@@ -22,13 +60,13 @@ $(function(){
 		function(){
 			console.log(_width);
 			if (_width < 1024) {
-				$(this).children(".trace-down").show();
+				$(this).children(".track-down").show();
 			}
 		},
 		function(){
 			console.log(_width);
 			if (_width < 1024) {
-				$(this).children(".trace-down").hide();
+				$(this).children(".track-down").hide();
 			}
 		}
 	);
@@ -39,7 +77,7 @@ $(function(){
 	function initPager(){
 		$.ajax({
           type: "GET",
-          url: "../txt/" + disk + "_" + trace + ".txt",
+          url: "../txt/" + disk + "_" + track + ".txt",
           success: function(data){
 		  	sectionCount = data.split("<br/>").length;
 			pageCount = (sectionCount - 1) / 5;
@@ -68,7 +106,7 @@ $(function(){
 	function showContext(currentPage){
 		$.ajax({
           type: "GET",
-          url: "../txt/" + disk + "_" + trace + ".txt",
+          url: "../txt/" + disk + "_" + track + ".txt",
           success: function(data){
 		  	console.log(data);
 			data = data.split("<br/>");
